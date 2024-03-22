4370d5b2cae7b584fc67b10e0f8b815bf8a7abba
Your NameMuhammad-Uzair11 <uzairamir315@gmail.com>
2024-03-23 01:02:48 +0500

    console add

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const randomnumber = Math.floor(Math.random() * 6 + 1);
+{
+    console.log("welcome to the number guessing game");
+}
 const answer = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "please guess the number",
+        message: "please guess the number 1-6",
     },
 ]);
 if (answer.userGuessedNumber === randomnumber) {
--- index.ts
+++ index.ts
@@ -4,12 +4,13 @@
 import inquirer from "inquirer";
 
 const randomnumber = Math.floor(Math.random() * 6 + 1);
+{console.log("welcome to the number guessing game")}
 
 const answer = await inquirer.prompt([
   {
     name: "userGuessedNumber",
     type: "number",
-    message: "please guess the number",
+    message: "please guess the number 1-6",
   },
 ]);
 
