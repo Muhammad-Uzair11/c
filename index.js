bd9d4f195305b58f0cbd6e5a743841aa2b909130
Your NameMuhammad-Uzair11 <uzairamir315@gmail.com>
2024-03-23 00:50:52 +0500

    my first number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess the number",
+    },
+]);
+if (answer.userGuessedNumber === randomnumber) {
+    console.log("congratulation! you guessed the right number");
+}
+else {
+    console.log("you guess wrong number");
+}
